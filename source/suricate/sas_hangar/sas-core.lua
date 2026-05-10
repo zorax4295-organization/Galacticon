1c9d71f29ef0fab4ccdf19bd4a1e66cc2a96efff
suricate <nils.blecua@gmail.com>
2026-05-10 19:05:06 +0200

    add: sens ExterInter + controle sas

--- source/suricate/sas_hangar/sas-core.lua
+++ source/suricate/sas_hangar/sas-core.lua
@@ -28,16 +28,18 @@ local LT = ic.enums.LogicType
 local LBM = ic.enums.LogicBatchMethod
 
 local sensor = 0
-local sensorextern = 1
-local sensorintern = 2
+local sensorIntern = 1
+local sensorExtern = 2
 local flashLight = hash("StructureFlashingLight")
-local hangarDoor = hash("StructureMediumHangerDoor")
-local hangarDoorInterName = ""
-local hangarDoorExterName = ""
-local poweredVentH = hash("StructurePoweredVent")
-local poweredVentInterName = ""
-local poweredVentExterName = ""
-local light = hash("StructureWallLight")
+local hangarDoor = hash("StructureGlassDoor")
+local hangarDoorInterName = hash("Glass Door inter")
+local hangarDoorExterName = hash("Glass Door exter")
+local poweredVentHash = hash("StructureActiveVent")
+local poweredVentInterName = hash("Active Vent inter")
+local poweredVentExterName = hash("Active Vent exter")
+local light = hash("StructureLightRound")
+
+local startCycle = false
 
 local state = {
     idle = 0,
@@ -54,41 +56,102 @@ local sensCycle = {
 local currentSensCycle = sensCycle.interExter
 
 
+----------------------
+---temporaire
+----------------------
+
+local buttonStartCyleHash = hash("ModularDeviceUtilityButton2x2")
+
+
+
 ----------------------------
 -- Init du système
 ----------------------------
 
-ic.batch_write(hangarDoor, LT.Lock, 1)
-ic.batch_write(flashLight, LT.Lock, 1)
+ic.batch_write(hangarDoor, LT.Lock, 0)
+ic.batch_write(flashLight, LT.Lock, 0)
 ic.batch_write(flashLight, LT.On, 0)
-ic.batch_write(light, LT.Lock, 1)
+ic.batch_write(light, LT.Lock, 0)
 ic.batch_write(light, LT.On, 1)
+ic.batch_write(poweredVentHash, LT.On, 0)
+
+----------------------------
+-- Définition des functions
+----------------------------
+
+local function cycleInterExter()
+    if currentState == state.interExterDepresurisation or currentState == state.idle then
+        currentState = state.interExterDepresurisation
+        ic.batch_write_name(hangarDoor, hangarDoorInterName, LT.Open, 0)
+        ic.batch_write(flashLight, LT.On, 1)
+        ic.batch_write_name(poweredVentHash, poweredVentInterName, LT.Mode, 1) -- Dépressuriser
+        ic.batch_write_name(poweredVentHash, poweredVentInterName, LT.On, 1)
+        while system.safe.read(sensor, LT.Pressure, "Gas Sensor") ~= 0 do yield() end -- Tant que la pression !=0 alors je patiente
+        ic.batch_write_name(poweredVentHash, poweredVentInterName, LT.On, 0)
+        yield()
+        currentState = state.interExterPresurisation
+    end
+    
+    if currentState == state.interExterPresurisation then
+        ic.batch_write_name(poweredVentHash, poweredVentExterName, LT.Mode, 0) -- Préssuriser
+        ic.batch_write_name(poweredVentHash, poweredVentExterName, LT.On, 1)
+        while 
+            system.safe.read(sensor, LT.Pressure, "Gas Sensor") ~= system.safe.read(sensorExtern, LT.Pressure, "Gas Sensor Extern") and
+            system.safe.read(sensorExtern, LT.Pressure, "Gas Sensor Extern") >=10 -- Supérieur a 10kPa
+        do
+            yield()
+        end
+        yield()
+        ic.batch_write_name(poweredVentHash, poweredVentExterName, LT.On, 0)
+        ic.batch_write_name(hangarDoor, hangarDoorExterName, LT.Open, 1)
+        ic.batch_write(flashLight, LT.On, 0)
+    end
+    currentSensCycle = sensCycle.exterInter
+end
+
+local function cycleExterInter()
+    if currentState == state.ExterInterDepresurisation or currentState == state.idle then
+        currentState = state.ExterInterDepresurisation
+        ic.batch_write_name(hangarDoor, hangarDoorExterName, LT.Open, 0)
+        ic.batch_write(flashLight, LT.On, 1)
+        ic.batch_write_name(poweredVentHash, poweredVentExterName, LT.Mode, 1) -- Dépressuriser
+        ic.batch_write_name(poweredVentHash, poweredVentExterName, LT.On, 1)
+        while system.safe.read(sensor, LT.Pressure, "Gas Sensor") ~= 0 do yield() end -- Tant que la pression !=0 alors je patiente
+        ic.batch_write_name(poweredVentHash, poweredVentExterName, LT.On, 0)
+        yield()
+        currentState = state.ExterInterPresurisation
+    end
+    
+    if currentState == state.ExterInterPresurisation then
+        ic.batch_write_name(poweredVentHash, poweredVentInterName, LT.Mode, 0) -- Pressuriser
+        ic.batch_write_name(poweredVentHash, poweredVentInterName, LT.On, 1)
+        while 
+            system.safe.read(sensor, LT.Pressure, "Gas Sensor") <= system.safe.read(sensorIntern, LT.Pressure, "Gas Sensor Extern")-0.01 and
+            system.safe.read(sensorIntern, LT.Pressure, "Gas Sensor Extern") >=10 -- Supérieur a 10kPa
+        do
+            yield()
+        end
+        yield()
+        ic.batch_write_name(poweredVentHash, poweredVentInterName, LT.On, 0)
+        ic.batch_write_name(hangarDoor, hangarDoorInterName, LT.Open, 1)
+        ic.batch_write(flashLight, LT.On, 0)
+    end
+    currentSensCycle = sensCycle.interExter
+end
+
 
 
 while true do
-    currentState = state.idle --Le système est en attente d'un lancement du cycle
-    local startCycle = true
-    
-    if startCycle then
-        currentState = state.interExterDepresurisation
-        ic.batch_write_name(hangarDoor, hangarDoorInterName, LT.Open, 0)
-        local hangarDoorOpenState = ic.batch_read(hangarDoor, LT.Open, LBM.Maximum)
-        if hangarDoorOpenState==1 then
-            print(system.log.time().."h "..system.log.level("fatal").." : Porte non fermer")
-            -- faire crash le programme
-        end
-        ic.batch_write(flashLight, LT.On, 1)
-        ic.batch_write_name(poweredVentH, poweredVentInterName, LT.Mode, 1) -- Dépressuriser
-        ic.batch_write_name(poweredVentH, poweredVentInterName, LT.On, 1)
-        while system.safe.read(sensor, LT.Pressure, "Gas Sensor") ~= 0 do yield() end -- Tant que la pression !=0 alors je patiente
-        ic.batch_write_name(poweredVentH, poweredVentInterName, LT.On, 0)
+    startCycle = ic.batch_read(buttonStartCyleHash, LT.Activate, LBM.Maximum)
 
-        currentState = state.interExterPresurisation
-        ic.batch_write_name(poweredVentH, poweredVentExterName, LT.Mode, 0) --Préssuriser
-        while system.safe(sensor, LT.Pressure, "Gas Sensor") ~=system.safe(sensorextern, LT.Pressure, "Gas Sensor Extern") do yield() end
-        ic.batch_write_name(hangarDoor, hangarDoorExterName, LT.Open, 1)
-         ic.batch_write(flashLight, LT.On, 0)
-        
+    if startCycle==1 then
+        if currentSensCycle == sensCycle.interExter then
+            cycleInterExter()
+        else
+            cycleExterInter()
+        end
     end
+
+    currentState = state.idle --Le système est en attente d'un lancement du cycle
     yield()
 end
